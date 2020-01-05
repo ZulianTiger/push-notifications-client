e0f5d221d8b99487d28d959620411c914b8a95a2
ZulianTiger <ady0606@gmail.com>
2020-01-05 13:16:16 +0100

    Updated gradle, installed FCM and notifications module and set up build tools

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
 
-AppRegistry.registerComponent('notifications', () => App)
+import bgMessaging from './App/Services/bgMessaging'
+
+AppRegistry.registerComponent('notifications', () => App);
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => bgMessaging);
